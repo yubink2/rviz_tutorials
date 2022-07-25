d02ee3863d39c32be21e6be96043a43135667187
yubink2 <yubink2@illinois.edu>
2022-07-25 19:38:29 -0400

    edited interactive marker tutorials

--- interactive_marker_tutorials/src/button_mode.cpp
+++ interactive_marker_tutorials/src/button_mode.cpp
@@ -174,82 +174,6 @@ double rand( double min, double max )
   return min + t*(max-min);
 }
 
-////////////////////////////////////////////////////////////////////////////////////
-
-// %Tag(6DOF)%
-void make6DofMarker( bool fixed, unsigned int interaction_mode, const tf::Vector3& position, bool show_6dof )
-{
-  InteractiveMarker int_marker;
-  int_marker.header.frame_id = "base_link";
-  tf::pointTFToMsg(position, int_marker.pose.position);
-  int_marker.scale = 1;
-
-  int_marker.name = "simple_6dof";
-  // int_marker.description = "Simple 6-DOF Control";
-
-  // insert a box
-  makeBoxControl(int_marker);
-  int_marker.controls[0].interaction_mode = interaction_mode;
-
-  InteractiveMarkerControl control;
-
-  if ( fixed )
-  {
-    int_marker.name += "_fixed";
-    // int_marker.description += "\n(fixed orientation)";
-    control.orientation_mode = InteractiveMarkerControl::FIXED;
-  }
-
-  if (interaction_mode != visualization_msgs::InteractiveMarkerControl::NONE)
-  {
-      std::string mode_text;
-      if( interaction_mode == visualization_msgs::InteractiveMarkerControl::MOVE_3D )         mode_text = "MOVE_3D";
-      if( interaction_mode == visualization_msgs::InteractiveMarkerControl::ROTATE_3D )       mode_text = "ROTATE_3D";
-      if( interaction_mode == visualization_msgs::InteractiveMarkerControl::MOVE_ROTATE_3D )  mode_text = "MOVE_ROTATE_3D";
-      int_marker.name += "_" + mode_text;
-      // int_marker.description = std::string("3D Control") + (show_6dof ? " + 6-DOF controls" : "") + "\n" + mode_text;
-  }
-
-  if(show_6dof)
-  {
-    tf::Quaternion orien(1.0, 0.0, 0.0, 1.0);
-    orien.normalize();
-    tf::quaternionTFToMsg(orien, control.orientation);
-    control.name = "rotate_x";
-    control.interaction_mode = InteractiveMarkerControl::ROTATE_AXIS;
-    int_marker.controls.push_back(control);
-    control.name = "move_x";
-    control.interaction_mode = InteractiveMarkerControl::MOVE_AXIS;
-    int_marker.controls.push_back(control);
-
-    orien = tf::Quaternion(0.0, 1.0, 0.0, 1.0);
-    orien.normalize();
-    tf::quaternionTFToMsg(orien, control.orientation);
-    control.name = "rotate_z";
-    control.interaction_mode = InteractiveMarkerControl::ROTATE_AXIS;
-    int_marker.controls.push_back(control);
-    control.name = "move_z";
-    control.interaction_mode = InteractiveMarkerControl::MOVE_AXIS;
-    int_marker.controls.push_back(control);
-
-    orien = tf::Quaternion(0.0, 0.0, 1.0, 1.0);
-    orien.normalize();
-    tf::quaternionTFToMsg(orien, control.orientation);
-    control.name = "rotate_y";
-    control.interaction_mode = InteractiveMarkerControl::ROTATE_AXIS;
-    int_marker.controls.push_back(control);
-    control.name = "move_y";
-    control.interaction_mode = InteractiveMarkerControl::MOVE_AXIS;
-    int_marker.controls.push_back(control);
-  }
-
-  server->insert(int_marker);
-  server->setCallback(int_marker.name, &processFeedback);
-  if (interaction_mode != visualization_msgs::InteractiveMarkerControl::NONE)
-    menu_handler.apply( *server, int_marker.name );
-}
-// %EndTag(6DOF)%
-
 // %Tag(Button)%
 void makeButtonMarker( const tf::Vector3& position, std::string str )
 {
@@ -281,28 +205,21 @@ void makeButtonMarker( const tf::Vector3& position, std::string str )
 // %Tag(main)%
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "basic_controls");
+  ros::init(argc, argv, "button_mode");
   ros::NodeHandle n;
 
   // create a timer to update the published transforms
   ros::Timer frame_timer = n.createTimer(ros::Duration(0.01), frameCallback);
 
-  server.reset( new interactive_markers::InteractiveMarkerServer("basic_controls","",false) );
+  server.reset( new interactive_markers::InteractiveMarkerServer("button_mode","",false) );
 
   ros::Duration(0.1).sleep();
 
   tf::Vector3 position;
-  position = tf::Vector3(-2, 0, 0);
-  make6DofMarker( false, visualization_msgs::InteractiveMarkerControl::ROTATE_3D, position, false );
-  position = tf::Vector3( 0, 0, 0);
-  make6DofMarker( false, visualization_msgs::InteractiveMarkerControl::MOVE_ROTATE_3D, position, true );
-  position = tf::Vector3( 2, 0, 0);
-  make6DofMarker( false, visualization_msgs::InteractiveMarkerControl::MOVE_3D, position, false );
-
-  // position = tf::Vector3( -1, 0, 0); 
-  // makeButtonMarker( position, "Test1" );
-  // position = tf::Vector3( 1, 0, 0); 
-  // makeButtonMarker( position, "Test2" );
+  position = tf::Vector3( -1, 0, 0); 
+  makeButtonMarker( position, "Test1" );
+  position = tf::Vector3( 1, 0, 0); 
+  makeButtonMarker( position, "Test2" );
 
 
   server->applyChanges();
